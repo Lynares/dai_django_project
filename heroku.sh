207a3e11888fd2efed892da38491ab0df8f1b6a0
Lynares <alvarlinares@gmail.com>
2016-02-09 19:18:44 +0100

    probando

--- heroku.sh
+++ heroku.sh
@@ -2,12 +2,11 @@
 
 wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh   
 	heroku login
-	heroku create
+	heroku create alvarobares
 	git add .
 	git commit -m "Despliegue heroku"
 	git push heroku master
 	heroku run python manage.py syncdb --noinput
 	heroku ps:scale web=1
-	heroku apps:rename alvarobares
 	heroku open
 
